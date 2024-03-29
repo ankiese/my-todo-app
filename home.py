9767e9a8d7195e6c3c5997275238617e34bd62a0
ankiese <ankiese@gmail.com>
2024-03-29 14:53:42 +0100

    Add About Page and Rename Web to Home

--- home.py
+++ home.py
@@ -3,6 +3,8 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide") #expands for mobile devices
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n" #object type that contains text from your program
     todos.append(todo)
@@ -11,7 +13,11 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity")
+st.write("This app is to increase your <b>productivity</b>.",
+         unsafe_allow_html=True)
+
+st.text_input(label="", placeholder="Add a new todo...",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,5 +28,3 @@ for index, todo in enumerate(todos):
         st.rerun()
 
 
-st.text_input(label="", placeholder="Add a new todo...",
-              on_change=add_todo, key='new_todo')
